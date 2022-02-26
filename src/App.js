bca12181e9d1526f2ddd7073d00021e80398a9df
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2022-02-27 01:01:23 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Project 2 Made by Hammad Bhatti</code> and save to reload.
         </p>
+        <br/> Second Project
         <a
           className="App-link"
           href="https://reactjs.org"
